a1b59fdbbd360c4cf7d6875ca20480cfd013ca92
volteeee <baymax732001@gmail.com>
2022-05-10 16:56:10 +0530

    atmost completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
 void main() {
-  return runApp(MaterialApp(
-    home: Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: TextButton(
-            onPressed: () {
-             
-              final player = AudioPlayer();
-              player.play('assets/note2.wav');
-            },
-            child: const Text("Click here"),
+  return runApp(XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({Key? key}) : super(key: key);
+
+  void playSound(int i) {
+    final player = AudioPlayer();
+    player.play('assets/note$i.wav');
+  }
+
+  Expanded buildkey({required Color color, required int soundNumber}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        child: Text(""),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildkey(color: Colors.red, soundNumber: 1), //1
+              buildkey(color: Colors.orange, soundNumber: 2), //2
+              buildkey(color: Colors.yellow, soundNumber: 3), //3
+              buildkey(color: Colors.green, soundNumber: 4), //4
+              buildkey(color: Colors.teal, soundNumber: 5), //5
+              buildkey(color: Colors.blue, soundNumber: 6), //6
+              buildkey(color: Colors.purple, soundNumber: 7), //7
+            ],
           ),
         ),
       ),
-    ),
-  ));
+    );
+  }
 }
